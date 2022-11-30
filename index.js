5677ef9ad628fbb9761f8ca59024c904c243135e
Paxyifier <59118083+Paxyifier@users.noreply.github.com>
2022-11-30 08:10:42 +0000

    exploring path module

---
+++ index.js
@@ -0,0 +1,7 @@
+const path = require('path')
+var filename = path.basename('/Workspaces/WTL-Prac3/index.js')
+var dname = path.dirname('/Workspaces/WTL-Prac3/index.js');
+var ename= path.extname('/Workspaces/WTL-Prac3/index.js');
+console.log(filename);
+console.log(dname);
+console.log(ename);
